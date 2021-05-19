3ba583dd969584c40883fe31b4d8888a87578e7f
Chydfile <48090062+Chydfile@users.noreply.github.com>
2021-05-19 23:50:14 +0300

    Update app_test.py

--- online_inference/app_test.py
+++ online_inference/app_test.py
@@ -1,8 +1,9 @@
 import pickle
 import pandas as pd
+import numpy as np
 from fastapi.testclient import TestClient
 from app import app
-
+import requests
 
 client = TestClient(app)
 
@@ -25,5 +26,20 @@ def test_result():
     prediction = model.predict(data)
     assert prediction is not None
     assert len(prediction) == len (data)
+
+
+def test_predict():
+    with TestClient(app) as client:
+        response = client.get("/predict")
+        data = pd.read_csv("data/x_test.csv")
+        request_features = list(data.columns)
+        for i in range(data.shape[0]):
+            request_data = [
+                x.item() if isinstance(x, np.generic) else x for x in data.iloc[i].tolist()
+            ]
+            response = client.get("/predict",
+                json={"data": [request_data], "features": request_features},
+            )
+            assert response.status_code == 200
 
         
